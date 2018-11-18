07c36e630fe2857aa7c93c0b0a7f54b0607c9796
3mdf1v3 <3mdf1v3@gmail.com>
2018-11-18 12:07:52 +0100

    Create tcp-flags-dissector.lua

---
+++ tcp-flags-dissector.lua
@@ -0,0 +1,43 @@
+local function DecodeFlag(flags, mask, character)
+	if bit.band(flags, mask) == 0 then
+		return '*'
+	else
+		return character
+	end
+end
+
+local function DefineAndRegisterTCPFlagsPostdissector()
+	local oProtoTCPFlags = Proto('tcpflags', 'TCP Flags Postdissector')
+
+	local oProtoFieldTCPFlags = ProtoField.string('tcpflags.flags', 'TCP Flags', 'The TCP Flags')
+
+	oProtoTCPFlags.fields = {oProtoFieldTCPFlags}
+
+	local oField_tcp_flags = Field.new('tcp.flags')
+
+	function oProtoTCPFlags.dissector(buffer, pinfo, tree)
+		local i_tcp_flags = oField_tcp_flags()
+		local s_tcp_flags = ''
+
+		if i_tcp_flags ~= nil then
+			s_tcp_flags = s_tcp_flags .. DecodeFlag(i_tcp_flags.value, 0x80, 'C')
+			s_tcp_flags = s_tcp_flags .. DecodeFlag(i_tcp_flags.value, 0x40, 'E')
+			s_tcp_flags = s_tcp_flags .. DecodeFlag(i_tcp_flags.value, 0x20, 'U')
+			s_tcp_flags = s_tcp_flags .. DecodeFlag(i_tcp_flags.value, 0x10, 'A')
+			s_tcp_flags = s_tcp_flags .. DecodeFlag(i_tcp_flags.value, 0x08, 'P')
+			s_tcp_flags = s_tcp_flags .. DecodeFlag(i_tcp_flags.value, 0x04, 'R')
+			s_tcp_flags = s_tcp_flags .. DecodeFlag(i_tcp_flags.value, 0x02, 'S')
+			s_tcp_flags = s_tcp_flags .. DecodeFlag(i_tcp_flags.value, 0x01, 'F')
+			local oSubtree = tree:add(oProtoTCPFlags, 'TCP Flags')
+			oSubtree:add(oProtoFieldTCPFlags, s_tcp_flags)
+		end
+	end
+
+	register_postdissector(oProtoTCPFlags)
+end
+
+local function Main()
+	DefineAndRegisterTCPFlagsPostdissector()
+end
+
+Main()
